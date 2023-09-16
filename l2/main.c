efa3d969647e7fc8fd97ea655b195fdf6ba75b8b
DuDuCoeio <130939493+DuDuCoeio@users.noreply.github.com>
2023-09-16 09:54:31 -0300

    Add files via upload

---
+++ l2/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Questão 1 - Escreva um programa em C que ordena um array de inteiros usando o algoritmo de ordenação por bolha.
+
+int main() {
+	
+	int i, aux, n, vetor[5]={7,3,5,6,4};
+	
+		
+for(n=0;n<=4;n++){	
+	 for(i=0;i<=3;i++){
+		
+		if(vetor[i] > vetor[i+1]){
+		   aux = vetor[i];
+		   vetor[i] = vetor[i+1];
+		   vetor[i+1] = aux;
+	  }
+	 }	
+	}
+	printf("\n");
+	for(i=0;i<=4;i++){
+		printf("%d \t",vetor[i]);
+	}
+	return 0;
+}
+*/
+
+/*
+Questão 2 - Compare o desempenho do algoritmo de ordenação por bolha com algoritmo de ordenação o quicksort e 
+o mergesort, usando arrays de tamanhos diferentes. Meça o tempo de execução e compare os resultado.
+*/
+
+int main(){
+	
+	
+	return 0;
+}
+
